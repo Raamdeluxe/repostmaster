2e51d72caf165211667d26de209f784fe8cbcfc4
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-14 14:55:28 +0100

    style navbar

--- client/src/components/sections/Navbar.js
+++ client/src/components/sections/Navbar.js
@@ -4,7 +4,10 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar__container}>
       <div className={styles.navbar__logo}>
-        <h1>RepostMaster</h1>
+        <h1>
+          <span style={{ color: "#8f5cf3" }}>Repost</span>
+          <span style={{ color: "#FE9799" }}>Master</span>
+        </h1>
       </div>
       <div className={styles.navbar__signup__button}>
         <button>Sign Up</button>

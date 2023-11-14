9999de40d9727f2362b1b282fe6d784cbaeb30f3
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-14 12:26:05 +0100

    set up homepagetemplate

--- client/src/components/templates/HomepageTemplate.js
+++ client/src/components/templates/HomepageTemplate.js
@@ -1,3 +1,23 @@
+import Navbar from "../sections/Navbar";
+import Hero from "../sections/Hero";
+import KeyFeatures from "../sections/KeyFeatures";
+import Testimonials from "../sections/Testimonials";
+import Announcement from "../sections/Announcement";
+import Pricing from "../sections/Pricing";
+import Waitlist from "../sections/Waitlist";
+import Footer from "../sections/Footer";
+
 export default function HomepageTemplate() {
-  return <div>HomepageTemplate</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <KeyFeatures />
+      <Testimonials />
+      <Announcement />
+      <Pricing />
+      <Waitlist />
+      <Footer />
+    </div>
+  );
 }

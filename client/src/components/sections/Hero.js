e997913d8ba1f47086a209b9842ff227a5ce32d3
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-14 16:18:07 +0100

    style hero section

--- client/src/components/sections/Hero.js
+++ client/src/components/sections/Hero.js
@@ -1,16 +1,21 @@
 import styles from "./Hero.module.css";
+import Robot from "../../assets/images/CU1TE_BOT_VI_03 1.png";
 
 export default function Hero() {
   return (
     <section className={styles.hero__container}>
       <div className={styles.hero__content__top}>
         <div className={styles.hero__title}>
-          <h2>The World's First Direct Message Auto Repost AI Bot</h2>
+          <h2>
+            <span style={{ color: "#8f5cf3" }}>The World's First</span>
+            <span style={{ color: "#FE9799" }}>Direct Message</span>
+            <span style={{ color: "#FE9799" }}> Auto Repost AI Bot</span>
+          </h2>
         </div>
         <div className={styles.hero__subtitle}>
           <h2>
-            Find. Send. Repost. Easily grow your social media with viral
-            content.
+            <span>Find. Send. Repost.</span>
+            <span>Easily grow your social media with viral content.</span>
           </h2>
         </div>
         <div className={styles.hero__signup__button}>
@@ -19,10 +24,19 @@ export default function Hero() {
       </div>
       <div className={styles.hero__content__bottom}>
         <div className={styles.hero__text}>
-          <h1>Monetize your time spent on funny reels</h1>
+          <h2>
+            <span style={{ color: "#FC969A" }}>
+              Monetize
+              <span style={{ color: "#FFF" }}> your</span>
+            </span>
+            <span style={{ color: "#FFF" }}> time spent on</span>
+            <span style={{ color: "#8657FA" }}>
+              <span style={{ color: "#FFF" }}> funny</span> reels
+            </span>
+          </h2>
         </div>
         <div className={styles.hero__image}>
-          <p>Image</p>
+          <img src={Robot} alt="ai_robot" />
         </div>
       </div>
     </section>

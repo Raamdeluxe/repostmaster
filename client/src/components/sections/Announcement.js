913f847d787909e5b52496d8aeee80c60473bb29
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-15 16:12:40 +0100

    style announcement section

--- client/src/components/sections/Announcement.js
+++ client/src/components/sections/Announcement.js
@@ -10,10 +10,16 @@ export default function Announcement() {
         <div className={styles.announcement__text}>
           <p>
             So if you are a theme page owner or content creator, dropshipper or
-            someone who needs to generate leads quickly and easy - do not
-            hesitate, only 10$/month for early adopters. Bot will be available
-            for public in Q1 2024. More features, like cross-platform reposting,
-            AI-driven reposts and other coming in Q2 2024
+            someone who needs to{" "}
+            <span style={{ color: "#FC969A" }}>
+              generate leads quickly and easy
+            </span>{" "}
+            - do not hesitate,{" "}
+            <span style={{ color: "#8757F9" }}>only 10$/month</span> for early
+            adopters. Bot will be available for public in{" "}
+            <span style={{ color: "#FC969A" }}>Q1 2024.</span> More features,
+            like cross-platform reposting, AI-driven reposts and other coming in
+            Q2 2024.
           </p>
         </div>
       </div>

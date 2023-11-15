60c939fd2bf4ef43e8d81e968e19bd53181ed28c
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-15 15:56:28 +0100

    style testimonials section

--- client/src/components/sections/Testimonials.js
+++ client/src/components/sections/Testimonials.js
@@ -1,4 +1,5 @@
 import Styles from "./Testimonials.module.css";
+import TestimonialsList from "../card/TestimonialsList";
 
 export default function Testimonials() {
   return (
@@ -8,26 +9,7 @@ export default function Testimonials() {
           <h2>Testimonials</h2>
         </div>
         <div className={Styles.testimonials__cards}>
-          <div className={Styles.testimonials__card}>
-            <div className={Styles.testimonials__card__text}>
-              <p>
-                When someone sends a post or reel in private messages, our bot
-                automatically reposts it to your connected account.
-              </p>
-            </div>
-            <div className={Styles.testimonials__card__profile}>
-              <div className={Styles.testimonials__card__profile__image}>
-                <p>Image</p>
-              </div>
-              <div className={Styles.testimonials__card_profile__name}>
-                <p>Name</p>
-                <p>Position</p>
-              </div>
-              <div className={Styles.testimonials__card_profile__review}>
-                <p>Review</p>
-              </div>
-            </div>
-          </div>
+          <TestimonialsList />
         </div>
       </div>
     </section>

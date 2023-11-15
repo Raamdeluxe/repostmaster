01f1799a1774992fd964f409f0f7e23d4b325443
Robin Konijnendijk <robinkonijnendijk@robins-mbp.home>
2023-11-15 21:03:53 +0100

    style price section

---
+++ client/src/components/card/PricingCard.js
@@ -0,0 +1,66 @@
+import styles from "./PricingCard.module.css";
+
+export default function PricingCard({ ...pricingData }) {
+  return (
+    <div className={styles.pricing__card}>
+      <div className={styles.pricing__icon__content}>
+        <div className={styles.pricing__icon}>
+          {pricingData.SvgComponent && <pricingData.SvgComponent />}
+        </div>
+      </div>
+      <div className={styles.pricing__title}>
+        <h3>{pricingData.title}</h3>
+      </div>
+      <hr className={styles.horizontal__line}></hr>
+      <ul className={styles.pricing__card__features}>
+        {pricingData.features.map((feature, index) => (
+          <div className={styles.pricing__card__features__content}>
+            <li key={index} className={styles.pricing__card__features__icon}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="9"
+                viewBox="0 0 9 9"
+                fill="none"
+              >
+                <rect
+                  x="7.07104"
+                  y="2.12132"
+                  width="2"
+                  height="7"
+                  rx="1"
+                  transform="rotate(45 7.07104 2.12132)"
+                  fill="white"
+                />
+                <rect
+                  x="4.94971"
+                  y="7.07106"
+                  width="2"
+                  height="5"
+                  rx="1"
+                  transform="rotate(135 4.94971 7.07106)"
+                  fill="white"
+                />
+              </svg>
+            </li>
+            <p>{feature}</p>
+          </div>
+        ))}
+      </ul>
+      <hr className={styles.horizontal__line}></hr>
+      <div className={styles.pricing__card__pricing}>
+        <div className={styles.pricing__card__price}>
+          <span className={styles.pricing__card__amount}>
+            {pricingData.price}
+          </span>
+          <span className={styles.pricing__card__month}>
+            {pricingData.pricePeriod}
+          </span>
+        </div>
+        <div className={styles.pricing__card__button}>
+          <button>Sign up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/sections/Pricing.js
+++ client/src/components/sections/Pricing.js
@@ -1,4 +1,5 @@
 import styles from "./Pricing.module.css";
+import PricingList from "../card/PricingList";
 
 export default function Pricing() {
   return (
@@ -6,45 +7,8 @@ export default function Pricing() {
       <div className={styles.pricing__title}>
         <h2>Pricing</h2>
       </div>
-      <div className={styles.pricing__card}>
-        <div className={styles.pricing__icon}>
-          <p>Icon</p>
-        </div>
-        <div className={styles.pricing__title}>
-          <h3>Level 1 - early adopters</h3>
-        </div>
-        <div className={styles.pricing__card__features}>
-          <ul>
-            <p>
-              Icon
-              <li>1 Instagam account</li>
-            </p>
-            <p>
-              Icon
-              <li>Up to 5 Team Members</li>
-            </p>
-            <p>
-              Icon
-              <li>Available Q4 2023</li>
-            </p>
-            <p>
-              Icon
-              <li>Filters</li>
-            </p>
-            <p>
-              Icon
-              <li>Scheduling</li>
-            </p>
-          </ul>
-        </div>
-        <div className={styles.pricing__card__pricing}>
-          <div className={styles.pricing__card__price}>
-            <h3>$10/month</h3>
-          </div>
-          <div className={styles.pricing__card__button}>
-            <button>Sign up</button>
-          </div>
-        </div>
+      <div className={styles.pricing__cards}>
+        <PricingList />
       </div>
     </section>
   );

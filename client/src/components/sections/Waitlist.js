cc77847e7d5b4bc0e157e67ef7b1a8a341b56d97
Robin <Robin@boldinnovations.io>
2023-11-16 11:30:37 +0100

    style waitlist section

--- client/src/components/sections/Waitlist.js
+++ client/src/components/sections/Waitlist.js
@@ -1,4 +1,5 @@
 import Styles from "./Waitlist.module.css";
+import World from "../../assets/images/world.png";
 
 export default function Waitlist() {
   return (
@@ -9,6 +10,10 @@ export default function Waitlist() {
         </div>
         <div className={Styles.waitlist__text}>
           <p>Join the waitlist now. Get notified about start.</p>
+        </div>
+
+        <div className={Styles.waitlist__image}>
+          <img src={World} alt="World" />
         </div>
       </div>
     </section>
